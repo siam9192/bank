1d27081090800f8df011ed7102b1882cb4c96cb4
siam9192 <ahsiam999@gmail.com>
2023-08-15 11:41:50 +0600

    hi

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ const password = document.getElementById("password");
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click",()=>{
     if(gmail.value === "siam99@gmail.com" && password.value === "matal120"){
-window.location.href = "../bank/bank.html" 
+window.location.href = "bank/bank.html" 
 console.log(password)
 console.log(gmail)
     }
